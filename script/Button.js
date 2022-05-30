9c7e96f843ae6c2ca2ce40e8a587bd9a610412fe
Dominik Koziak <dkz@connecto.pl>
2022-05-30 14:53:42 +0200

    DEV 2205301453 HOTFIX

--- script/Button.js
+++ script/Button.js
@@ -1,28 +1,35 @@
 export class Button {
-    constructor(text, width, height, onClick = false, gridArea = false) {
-        this.node = document.createElement("button");
-        if (gridArea) this.node.style.gridArea = gridArea;
-        this.node.style.width = height;
-        this.node.style.height = width;
-        this.node.textContent = text;
-        if (onClick == "cancel") {
-            this.node.onclick = this.cancel;
-        } else if (onClick == "save") this.node.onclick = this.save;
-        else if (onClick) this.node.onclick = onClick;
-        return this.node;
-    }
+	constructor(text, width, height, onClick = false, gridArea = false) {
+		this.node = document.createElement("button");
+		if (gridArea) {
+			this.node.style.gridArea = gridArea;
+		}
+		this.node.style.width = height;
+		this.node.style.height = width;
+		this.node.textContent = text;
+		if (onClick == "cancel") {
+			this.node.onclick = this.cancel;
+		} else if (onClick == "save") {
+			this.node.onclick = this.save;
+		} else if (onClick) {
+			this.node.onclick = onClick;
+		}
+		return this.node;
+	}
 
-    cancel = function () {
-        const TL_BOX = this.parentElement.parentElement;
-        const MAIN_BOX = TL_BOX.parentElement;
-        MAIN_BOX.removeChild(TL_BOX);
-    };
+	cancel = function () {
+		console.log("asd");
+		const TL_BOX = this.parentElement.parentElement;
+		const MAIN_BOX = TL_BOX.parentElement;
+		MAIN_BOX.removeChild(TL_BOX);
+	};
 
-    save = function () {
-        this.parentElement.childNodes[2].style.visibility = "hidden";
-        this.parentElement.childNodes[1].style.visibility = "hidden";
-    };
+	save = function () {
+		console.log("asd");
+		this.parentElement.childNodes[2].style.visibility = "hidden";
+		this.parentElement.childNodes[1].style.visibility = "hidden";
+	};
 }
 export function test() {
-    console.log("TEST");
+	console.log("TEST");
 }
--- script/mainScript.js
+++ script/mainScript.js
@@ -1,9 +1,5 @@
-import {
-    TextArea
-} from "./TextArea.js";
-import {
-    Button
-} from "./Button.js";
+import { TextArea } from "./TextArea.js";
+import { Button } from "./Button.js";
 
 const MAIN_BOX = document.getElementById("mainBox");
 const SAVE_BTN = new Button("Save", "30px", "100%", "save", "2/1/2/2");
@@ -13,40 +9,52 @@ const TA = new TextArea();
 TA.addEventListener("input", onInput, false);
 
 function appendWithMinorBox(element) {
-    const MINOR_BOX = document.createElement("div");
-    MINOR_BOX.classList.add("minorBox");
-    MINOR_BOX.appendChild(element);
-    MAIN_BOX.insertBefore(
-        MINOR_BOX
-        , MAIN_BOX.childNodes[MAIN_BOX.childNodes.length - 1]
-    );
+	const MINOR_BOX = document.createElement("div");
+	MINOR_BOX.classList.add("minorBox");
+	MINOR_BOX.appendChild(element);
+	MAIN_BOX.insertBefore(
+		MINOR_BOX,
+		MAIN_BOX.childNodes[MAIN_BOX.childNodes.length - 1],
+	);
 }
 
 function addTask() {
-    const TASK = document.createElement("div");
-    TASK.classList.add("tlDiv");
-    //TASK.style.height = TA.scrollHeight + 50 + "px";
-    const _TA = TA.cloneNode(true);
-    _TA.addEventListener("input", onInput, false);
-    TASK.appendChild(_TA);
-    const _SAVE_BTN = SAVE_BTN.cloneNode(true);
-    _SAVE_BTN.onclick = SAVE_BTN.onclick;
-    TASK.appendChild(_SAVE_BTN);
-    const _CANCEL_BTN = CANCEL_BTN.cloneNode(true);
-    _CANCEL_BTN.onclick = CANCEL_BTN.onclick;
-    TASK.appendChild(_CANCEL_BTN);
-    console.log(CANCEL_BTN.onclick);
-    appendWithMinorBox(TASK);
+	const TASK = document.createElement("div");
+	TASK.classList.add("tlDiv");
+	//TASK.style.height = TA.scrollHeight + 50 + "px";
+	const _TA = TA.cloneNode(true);
+	_TA.addEventListener("blur", onBlur, false);
+	_TA.addEventListener("focus", onFocus, false);
+	_TA.addEventListener("input", onInput, false);
+	TASK.appendChild(_TA);
+	const _SAVE_BTN = SAVE_BTN.cloneNode(true);
+	_SAVE_BTN.onclick = SAVE_BTN.onclick;
+	TASK.appendChild(_SAVE_BTN);
+	const _CANCEL_BTN = CANCEL_BTN.cloneNode(true);
+	_CANCEL_BTN.onclick = CANCEL_BTN.onclick;
+	TASK.appendChild(_CANCEL_BTN);
+	appendWithMinorBox(TASK);
+}
+function test() {
+	console.log("asd");
 }
 
 function onInput() {
-    console.log(`He?`);
-    this.parentNode.style.height = this.scrollHeight + 30 + "px";
-    this.parentNode.style.width = this.scrollWidth + "px";
-    this.style.height = "50";
-    this.style.height = this.scrollHeight + "px";
-    this.style.width = "50";
-    this.style.width = this.scrollWidth + "px";
+	this.parentNode.style.height = this.scrollHeight + 30 + "px";
+	this.parentNode.style.width = this.scrollWidth + "px";
+	this.style.height = "50";
+	this.style.height = this.scrollHeight + "px";
+	this.style.width = "50";
+	this.style.width = this.scrollWidth + "px";
+}
+
+function onBlur() {
+	this.parentNode.childNodes[1].style.visibility = "hidden";
+	this.parentNode.childNodes[2].style.visibility = "hidden";
+}
+function onFocus() {
+	this.parentNode.childNodes[1].style.visibility = "visible";
+	this.parentNode.childNodes[2].style.visibility = "visible";
 }
 
 appendWithMinorBox(ADD_BUTTON);
